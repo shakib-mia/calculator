55e487a38b5b9c126742116c96996d26d8726865
shakib-mia <smdshakibmia2001@gmail.com>
2022-02-15 12:27:20 +0600

    functionality added

---
+++ index.js
@@ -0,0 +1,13 @@
+let inputField = document.getElementById('input');
+
+document.getElementById('keypad').addEventListener('click', function (event) {
+      inputField.value = inputField.value + event.target.value;
+})
+
+document.getElementById('equals').addEventListener('click', function () {
+      inputField.value = eval(inputField.value)
+})
+
+document.getElementById('clear').addEventListener('click', function () {
+      inputField.value = ''
+})
