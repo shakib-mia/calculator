2bbed30b5565fabee77e1b9e3c43e6187e06a955
shakib-mia <smdshakibmia2001@gmail.com>
2022-02-15 21:50:45 +0600

    digit number problem solved

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,14 @@
 let inputField = document.getElementById('input');
 
 document.querySelector('#keypad').addEventListener('click', function (event) {
-      inputField.value = inputField.value + event.target.value
+      inputField.value = inputField.value + event.target.value;
 })
 
 document.getElementById('equals').addEventListener('click', function () {
-      inputField.value = eval(inputField.value).toFixed(10);
-
+      inputField.value = eval(inputField.value);
+      if (inputField.value.includes('.') && inputField.value.length > 12) {
+            inputField.value = eval(inputField.value).toFixed(11);
+      }
 })
 
 document.getElementById('clear').addEventListener('click', function () {
@@ -14,5 +16,5 @@ document.getElementById('clear').addEventListener('click', function () {
 })
 
 document.getElementById('sqrt').addEventListener('click', function () {
-      inputField.value = Math.sqrt(eval(inputField.value)).toFixed(12)
+      inputField.value = Math.sqrt(eval(inputField.value)).toFixed(11);
 })
