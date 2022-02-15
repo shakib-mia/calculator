6729d2aed7508299880e4bd6da3d70377733c9c9
shakib-mia <smdshakibmia2001@gmail.com>
2022-02-15 13:09:35 +0600

    design and functionality improved

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,11 @@
 let inputField = document.getElementById('input');
 
 document.getElementById('keypad').addEventListener('click', function (event) {
-      inputField.value = inputField.value + event.target.value;
+      inputField.value = inputField.value + event.target.value
 })
 
 document.getElementById('equals').addEventListener('click', function () {
-      inputField.value = eval(inputField.value)
+      inputField.value = eval(inputField.value);
 })
 
 document.getElementById('clear').addEventListener('click', function () {
