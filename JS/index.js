6f3b93e251a39c5c6d2da87a0186c547c13cf5c5
Shakib Mia <87817870+shakib-mia@users.noreply.github.com>
2022-03-15 18:27:14 +0600

    blank click avoided

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,8 @@
 let inputField = document.getElementById('input');
 
 document.querySelector('#keypad').addEventListener('click', function (event) {
-      inputField.value = inputField.value + event.target.value;
+      if (event.target.value != undefined)
+            inputField.value = inputField.value + event.target.value;
 })
 
 document.getElementById('equals').addEventListener('click', function () {
@@ -16,8 +17,12 @@ document.getElementById('clear').addEventListener('click', function () {
 })
 
 document.getElementById('sqrt').addEventListener('click', function () {
-      inputField.value = Math.sqrt(eval(inputField.value));
-      if (inputField.value.includes('.') && inputField.value.length > 12) {
-            inputField.value = eval(inputField.value).toFixed(11);
+      if (inputField.value != "") {
+            inputField.value = Math.sqrt(eval(inputField.value));
+            if (inputField.value.includes('.') && inputField.value.length > 12) {
+                  inputField.value = eval(inputField.value).toFixed(11);
+            }
+      } else {
+            alert('Input field cannot be empty')
       }
 })
